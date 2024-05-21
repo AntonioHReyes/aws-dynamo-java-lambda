62d244177fe904523e830d1de114a542cd105f61
Antonio Huerta Reyes <anhr9728@gmail.com>
2024-05-20 20:06:08 -0600

    Initial commit

---
+++ src/main/java/com/tonyakitori/aws/PersonResponse.java
@@ -0,0 +1,13 @@
+package com.tonyakitori.aws;
+
+public class PersonResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
